4cc315d1dfc7409abb6197b29e28b03bea017ea2
Hanyoung Jo <sin098@naver.com>
2022-01-26 18:00:58 +0900

    한영수정본!

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -16,7 +16,7 @@ public class DemoController {
     //@RequestMapping(value = "/demo" , method = RequestMethod.GET)
     @GetMapping(value = "/demo")
     public ModelAndView demo(ModelAndView mv, String id){
-        log.info("?????????????????????조한영 수정 Test2");
+        log.info("내가내가내가내가내가내가내가내가 수정!");
 
         String name = "";
         if(id.equals("liu")){
